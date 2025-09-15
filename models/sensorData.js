2d9806e7d4a562f38ecb0d38e8a193ccdd9bac93
yash1915 <157246927+yash1915@users.noreply.github.com>
2025-09-15 21:57:33 +0530

    Update sensorData.js

--- models/sensorData.js
+++ models/sensorData.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const sensorDataSchema = new mongoose.Schema({
-    mq2: { type: Number, required: true },
-    temperature: { type: Number, required: true },
-    humidity: { type: Number, required: true },
-    pir: { type: Boolean, required: true },
-    ir: { type: Boolean, required: true },
+    mq2: { type: Number, required: false },
+    temperature: { type: Number, required:  false},
+    humidity: { type: Number, required:  false },
+    pir: { type: Boolean, required:  false },
+    ir: { type: Boolean, required:  false },
     timestamp: { type: Date, default: Date.now },
 });
 
